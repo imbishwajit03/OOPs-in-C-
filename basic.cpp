ae4f1476f2042dc4b2cd1fa798550030f9710443
imbishwajit03 <bishwajitgorai195@gmail.com>
2024-12-14 19:12:34 +0530

    basic code complete

--- basic.cpp
+++ basic.cpp
@@ -4,18 +4,42 @@ using namespace std;
 
 class Teacher
 {
+private:
+    double salary; // data hiding
+
 public:
+    // properties/attributes
     string firstName;
     string subject;
     string dept;
-    double salary;
+
+    // member funcion / methods
+    void changeSub(string newSubject)
+    {
+        subject = newSubject;
+    }
+
+    // Salary ko humlog indirectly public function ke through access karenge.
+    // setter (setter function ka kaam hota hay private value ko set karna)
+    void setSalary(double s)
+    {
+        salary = s;
+    }
+    // getter (getter function ka kaam hota hay private value ko display karwana)
+    double getSalary()
+    {
+        return salary;
+    }
 };
 
 int main()
 {
     Teacher t1;
-    t1.firstName="Bishwajit";
+    t1.firstName = "Bishwajit";
     t1.subject = "Dsa";
+    t1.setSalary(50000);
 
+    cout << t1.firstName << endl;
+    cout << t1.getSalary() << endl;
     return 0;
 }
